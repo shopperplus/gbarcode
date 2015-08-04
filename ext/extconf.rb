33c6e9a5974c9aac006a8dabae0ba56a317515d5
Cichol <cichol@live.cn>
2015-08-04 22:39:38 +0800

    remove -Werror=format-security flag avoiding compile error

--- ext/extconf.rb
+++ ext/extconf.rb
@@ -4,3 +4,7 @@
 #  Copyright (c) 2007. All rights reserved.
 require 'mkmf'
 create_makefile('gbarcode')
+# 避免gcc 4.9.1下的编译错误（也与mkmf生成的Makefile有关）
+makefile = File.open('Makefile').read
+makefile.gsub!('-Werror=format-security', '')
+File.open('Makefile', 'w').write makefile
